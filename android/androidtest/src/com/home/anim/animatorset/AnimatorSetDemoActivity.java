d95d1bc9225b44118a876c7daf9a2eb368c14a8a
jianwen.zhu <cooliszhu@gmail.com>
2014-07-16 16:22:39 +0800

    删除无用文件

---
+++ android/androidtest/src/com/home/anim/animatorset/AnimatorSetDemoActivity.java
@@ -0,0 +1,11 @@
+package com.home.anim.animatorset;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnimatorSetDemoActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
